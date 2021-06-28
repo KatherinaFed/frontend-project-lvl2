9e0ec1ffb7ca261454f4e59b51231cc771d3abaa
Katherina <fedoseeva2292@gmail.com>
2021-06-28 15:29:36 +0200

    second-step (added option, argument)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,7 @@ import program from 'commander';
 
 program
   .version('0.0.1', '-v, --version', 'output the version number')
+  .argument('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  // .argument('<filepath1> <filepath2>')
-  .action(() => {
-    console.log('Hello!');
-  })
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
