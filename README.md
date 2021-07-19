2fc14e0a8235183fe19c6161bfa883fb5f86eecb
Katherina <fedoseeva2292@gmail.com>
2021-07-19 12:17:01 +0200

    last step (json format)

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
+### Hexlet tests and linter status:
 [![Maintainability](https://api.codeclimate.com/v1/badges/7b0f4a5d3858c6ea7a12/maintainability)](https://codeclimate.com/github/KatherinaFed/frontend-project-lvl2/maintainability) [![Github actions](https://github.com/KatherinaFed/frontend-project-lvl2/actions/workflows/github-actions.yml/badge.svg)](https://github.com/KatherinaFed/frontend-project-lvl2/actions/workflows/github-actions.yml) [![Test Coverage](https://api.codeclimate.com/v1/badges/7b0f4a5d3858c6ea7a12/test_coverage)](https://codeclimate.com/github/KatherinaFed/frontend-project-lvl2/test_coverage) [![Actions Status](https://github.com/KatherinaFed/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/KatherinaFed/frontend-project-lvl2/actions)
 
 ### How it works.
 
-https://asciinema.org/a/2rbh12SHNKgMLSVimV2lOOoxw - Comparing JSON files.
+https://asciinema.org/a/axo8l9UjByElDSlGUTuPDA1To - Comparing STYLISH (default) format (json/yml).
 
-https://asciinema.org/a/2rbh12SHNKgMLSVimV2lOOoxw - Comparing YML files.
+https://asciinema.org/a/xf3wlYALsF821X1gR1xXWdWYn - Comparing PLAIN format (json/yml).
 
-https://asciinema.org/a/2vKXqbeWXNN5dSWxDhRhWSrIF - Comparing nested files.
-
-https://asciinema.org/a/t6F68QCO2FAiDAlqxoqWUGbNl - Comparing plain files.
+https://asciinema.org/a/cgzgo5eW0JzPD8fdnJkRKiRRb - Comparing JSON format (json/yml).
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,28 +2,41 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const jsonPath = path.resolve(process.cwd(), '__fixtures__/json.txt');
-const expectedJson = fs.readFileSync(jsonPath, 'utf-8');
+const stylishPath = path.resolve(process.cwd(), '__fixtures__/stylish.txt');
+const expectedStylish = fs.readFileSync(stylishPath, 'utf-8');
 
 const plainPath = path.resolve(process.cwd(), '__fixtures__/plain.txt');
 const expectedPlain = fs.readFileSync(plainPath, 'utf-8');
 
+const jsonPath = path.resolve(process.cwd(), '__fixtures__/json.txt');
+const expectedJson = fs.readFileSync(jsonPath, 'utf-8');
+
 // Compare JSON Files
-test('compare_json', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedJson);
+test('JSON stylish', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedStylish);
 });
 
 // Compare YAML/YML Files
-test('compare_yaml/yml', () => {
-  expect(genDiff('file1.yml', 'file2.yml')).toEqual(expectedJson);
+test('YAML stylish', () => {
+  expect(genDiff('file1.yml', 'file2.yml')).toEqual(expectedStylish);
 });
 
 // Compare JSON Files (plain)
-test('compare_json_plain', () => {
+test('JSON plain', () => {
   expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(expectedPlain);
 });
 
 // Compare YAML/YML Files (plain)
-test('compare_yaml/yml_plain', () => {
+test('YAML plain', () => {
   expect(genDiff('file1.yml', 'file2.yml', 'plain')).toEqual(expectedPlain);
+});
+
+// Compare JSON Files (json)
+test('JSON json', () => {
+  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJson);
+});
+
+// Compare YAML/YML Files (json)
+test('YAML json', () => {
+  expect(genDiff('file1.yml', 'file2.yml', 'json')).toEqual(expectedJson);
 });
